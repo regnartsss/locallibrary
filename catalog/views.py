3db36e033d5868fd611c10c42870334359b8550b
reg <regnartsss@gmail.com>
2020-07-29 11:41:51 +0700

    v2

--- catalog/views.py
+++ catalog/views.py
@@ -5,113 +5,107 @@ from datetime import datetime, timedelta
 # Create your views here.
 # from .models import Book, Author, BookInstance, Genre
 global stat, dat
+import sqlite3
 
-def open_all():
-    global stat, dat
 
-    with open(r'C:\GitHub\snmpvs\stat.json', 'rb') as f:
-        stat = json.load(f)
-    with open(r'C:\GitHub\snmpvs\dat.json', 'rb') as f:
-        dat = json.load(f)
+def sql_select(request):
+    conn = sqlite3.connect(r'C:\Users\podkopaev.k\PycharmProjects\snmpvs\work\sdwan.db')
+    cursor = conn.cursor()
+    cursor.execute(request)
+    rows = cursor.fetchall()
+    return rows
 
-    # with open(r'C:\Users\podkopaev.k\PycharmProjects\snmpvs\stat.json', 'rb') as f:
-    #     stat = json.load(f)
-    # with open(r'C:\Users\podkopaev.k\PycharmProjects\snmpvs\dat.json', 'rb') as f:
-    #     dat = json.load(f)
 
 def data_monitor():
     return datetime.today().strftime("%H:%M:%S %d/%m/%Y")
 
+
 def index(request):
-    open_all()
     r = 0
     tab = 0
     s = {}
-    stat_one = (sorted(dat.items(), key=lambda k: k[1]["region"]))
-    for key in stat_one:
-
-        if key[1]["region"] == 0:
+    # stat_one = (sorted(dat.items(), key=lambda k: k[1]["region"]))
+    req = "SELECT filial.kod, region, name, status_1, status_2, ISP1, ISP2  FROM filial LEFT JOIN status ON filial.kod = status.kod ORDER BY name"
+    # print(req)
+    rows = sql_select(req)
+    for row in rows:
+        print(row)
+        st1, st2 = status(row[3], row[4], row[5], row[6])
+        temp = [row[0], row[2], st1, st2]
+        if row[1] == 0:
             try:
-                s[0].append(key[0])
+                s[0].append(temp)
             except:
                 s[0] = []
-                s[0].append(key[0])
-        elif key[1]["region"] == 1:
+                s[0].append(temp)
+        elif row[1] == 1:
             try:
-                s[1].append(key[0])
+                s[1].append(temp)
             except:
                 s[1] = []
-                s[1].append(key[0])
-        elif key[1]["region"] == 2:
+                s[1].append(temp)
+        elif row[1] == 2:
             try:
-                s[2].append(key[0])
+                s[2].append(temp)
             except:
                 s[2] = []
-                s[2].append(key[0])
-        elif key[1]["region"] == 3:
+                s[2].append(temp)
+        elif row[1] == 3:
             try:
-                s[3].append(key[0])
+                s[3].append(temp)
             except:
                 s[3] = []
-                s[3].append(key[0])
-        elif key[1]["region"] == 4:
+                s[3].append(temp)
+        elif row[1] == 4:
             try:
-                s[4].append(key[0])
+                s[4].append(temp)
             except:
                 s[4] = []
-                s[4].append(key[0])
-        elif key[1]["region"] == 5 or key[1]["region"] == 6:
+                s[4].append(temp)
+        elif row[1] == 5 or row[1] == 6:
             try:
-                s[5].append(key[0])
+                s[5].append(temp)
             except:
                 s[5] = []
-                s[5].append(key[0])
-        # elif key[1]["region"] == 6:
-        #     try:
-        #         s[6].append(key[0])
-        #     except:
-        #         s[6]=[]
-        #         s[6].append(key[0])
-        elif key[1]["region"] == 7 or key[1]["region"] == 8 or key[1]["region"] == 9:
+                s[5].append(temp)
+        elif row[1] == 7 or row[1] == 8 or row[1] == 9:
             try:
-                s[7].append(key[0])
+                s[7].append(temp)
             except:
                 s[7] = []
-                s[7].append(key[0])
+                s[7].append(temp)
 
     # print(s)
 
+    # kod = s
     le = {}
     for l in s:
         le[l] = len(s[l])
-
     sort = sorted(le.items(), key=lambda k: -k[1])
-    # print(sort)
-    # print(sort[0][1])
     ll = sort[0][1]
-    # for r in sort:
-    #     print(s[r[0]])
-    #
+    sort = sorted(le.items(), key=lambda k: k[0])
+    print(ll)
+    kod = s
     i = 0
-
     tab = {}
-
     while i < ll:
-
         try:
             tab[i]
-        except:
+        except KeyError:
             tab[i] = []
-
+            # print(sort)
         for ss in sort:
+            print(ss)
+            # print(s[ss[0]])
+            print(s[ss[0]][0])
             try:
-#                tab[i].append(s[ss[0]][i])
-#                 print(stat[s[ss[0]][i]])
-                status(tab[i], stat[s[ss[0]][i]], s[ss[0]][i])
-            except:
+                tab[i].append(s[ss[0]][i])
+# #                 print(stat[s[ss[0]][i]])
+# #                 status(tab[i], stat[s[ss[0]][i]], s[ss[0]][i])
+            except IndexError:
                 tab[i].append("")
         i += 1
-    # print(tab)
+# # #     # print(tab)
     kod = tab
 
     return render(
@@ -120,24 +114,25 @@ def index(request):
         context={'kod': kod, "time":data_monitor()},
     )
 
-def status(tab, sss, k):
-            if sss["status_t1"] == 1:
-                ch1 = "🔵"
-            elif sss["status_t1"] == 0:
-                ch1 = "🔴"
-            if sss["status_t2"] == 1:
-                ch2 = "🔵"
-            elif sss["status_t2"] == 0:
-                ch2 = "🔴"
-            if dat[str(k)]["ISP1"] == "unassigned":
-                ch1 = "⚪"
-            if dat[str(k)]["ISP2"] == "unassigned":
-                ch2 = "⚪"
-            try:
-                tab.append([ch1, ch2, k, dat[str(k)]["name"]])
-            except:
-                tab = []
-                tab.append([ch1, ch2, k, dat[str(k)]["name"]])
+
+# index()
+
+
+def status(s1, s2, ISP1, ISP2):
+    ch1, ch2 = '🟡','🟡'
+    if s1 == 1:
+        ch1 = "🔵"
+    elif s1 == 0:
+        ch1 = "🔴"
+    if s2 == 1:
+        ch2 = "🔵"
+    elif s2 == 0:
+        ch2 = "🔴"
+    if ISP1 == "unassigned":
+        ch1 = "⚪"
+    if ISP2 == "unassigned":
+        ch2 = "⚪"
+    return ch1, ch2
 
 
         #
