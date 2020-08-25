863d2f15ca58362080032d4b586c0b55189faf86
reg <regnartsss@gmail.com>
2020-08-25 16:03:32 +0700

    v3 cd

--- catalog/views.py
+++ catalog/views.py
@@ -86,11 +86,14 @@ def index(request):
     num = int(len(rows)/7)
     i = 0
     s_i = 0
-    req = "SELECT filial.kod, name, down, disk, ip FROM filial LEFT JOIN registrator ON filial.kod = registrator.kod ORDER BY name"
+    req = "SELECT filial.kod, name, down, disk, ip, cam, cam_down FROM filial LEFT JOIN registrator ON filial.kod = registrator.kod ORDER BY name"
     rows = sql_select(req)
-
     # rows = sql_select(f"SELECT down FROM registrator WHERE kod = {row[0]}")
     for row in rows:
+        if row[5] == row[6]:
+            cam = "🟩"
+        else:
+            cam = "🟧"
         if row[2] == 1:
             reg, d = "🟥", "🟥"
         else:
@@ -99,7 +102,7 @@ def index(request):
         # name = f"{row[0]} {row[1]}"
         name = row[1]
         name = f" {name[:18]}"
-        temp = [name, reg, d, row[4]]
+        temp = [name, reg, d, row[4], cam]
         if i == num:
             i = 0
             s_i += 1
