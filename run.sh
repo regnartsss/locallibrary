ee52098d2f26c22319aab3ba29b9971b2a5c2dab
regnartsss <4Kikbtyma>
2021-02-16 21:59:17 +0700

    new_snmp

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/user/git/locallibrary/newenv/bin/python3 /home/user/git/locallibrary/manage.py runserver 0.0.0.0:8000
+/home/user/git/locallibrary/newenv/bin/python3 /home/user/git/locallibrary/manage.py runserver 0.0.0.0:8000  > file.log
