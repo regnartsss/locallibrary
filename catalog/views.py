401f89bdc454756c7a9ec9adee44ba792ed2aa9e
regnarts <regnartsss@gmail.com>
2022-02-21 10:02:06 +0700

    new_snmp

--- catalog/views.py
+++ catalog/views.py
@@ -33,7 +33,6 @@ charset = 'utf8mb4'
 def bd_fetchall(request):
     conn = mysql.connector.connect(host=HOST, port=PORT, user=USER, password=PASSWORD, db=DB, charset=charset)
     with conn.cursor() as cursor:
-        # cursor = await conn.cursor()
         cursor.execute(request)
         return cursor.fetchall()
 
