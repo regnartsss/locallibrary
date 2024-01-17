72fa10ba1a9dfff6828ef97cb74bee8c59eabbcb
regnarts <regnartsss@gmail.com>
2024-01-17 13:14:00 +0700

    new_snmp

--- catalog/views.py
+++ catalog/views.py
@@ -185,10 +185,10 @@ def status(s1, s2, sdwan, linkgi0, linkgi1, linktu1, lte, ssh_protocol_001, ssh_
         ch2 = "🔴"
     if linkgi0 == 2:
         ch1 = "🔵"
-    if linkgi1 == 2 and ssh_protocol_001 == 0 and ssh_protocol_tu1 == 0:
-        ch2 = "🔵"
     if ssh_protocol_001 == 0 and ssh_protocol_tu1 == 0:
         ch2 = "✔️"
+        if linkgi1 == 2 and ssh_protocol_001 == 0 and ssh_protocol_tu1 == 0:
+            ch2 = "🔵"
     if linktu1 == 2 and linkgi1 == 2 and lte == 1:
         ch2 = "🟣"
     # if linktu20 == 1 and linkgi1 == 2:
